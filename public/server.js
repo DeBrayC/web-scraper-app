2ea97c626b3b025c338507916be31e5d96688d95
DeBray Carpenter <debraycarpenter@DeBrays-MacBook-Pro.local>
2017-04-17 11:32:56 -0700

    added server.js file

---
+++ public/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const fs = require('fs')
+const request = require('request')
+const cheerio = require('cheerio')
+const app = express()
+
+app.get('/scrape', (req, res) => {
+
+
+
+})
+
+app.listen('8081')
+
+console.log('This is where the magic happens, port 8081')
+
+exports = module.exports = app
