851a56800c9b1540c5d1428b7974ec1514c74da8
DeBray Carpenter <debraycarpenter@DeBrays-MacBook-Pro.local>
2017-04-17 17:08:07 -0700

    updated server.js file

--- public/server.js
+++ public/server.js
@@ -6,12 +6,34 @@ const app = express()
 
 app.get('/scrape', (req, res) => {
 
+url = 'http://afdah.to/category/hd/'
 
+request(url, (error, response, html) => {
+
+if(!error){
+  let $ = cheerio.load(html)
+
+  let title, url
+  let json = { title : "", url : "" }
+
+  $('.entry-title').filter( () => {
+
+    let data = $(this)
+
+    url = data.children().first().href()
+    title = data.children().last().text()
+
+
+  })
+
+}
+
+})
 
 })
 
 app.listen('8081')
 
-console.log('This is where the magic happens, port 8081')
+console.log('Port 8081, where ALL the Magic Happens...')
 
-exports = module.exports = app
+exports = module exports = app
